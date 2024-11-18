4939a9fef194dfd75f3f95054c35d1348a941a71
ynawinraaj <gomsyuva19@gmail.com>
2024-11-18 20:51:26 +0530

    Add files via upload

---
+++ o1time.py
@@ -0,0 +1,8 @@
+def prime_number(n):
+    iteration=0
+    print("the entered by the user is: ","n")
+    iteration+=1
+    print("total iteration done by the code is: ",iteration)
+prime_number(7)
+prime_number(13)
+print("with any n the tim taken by the code go to change")
